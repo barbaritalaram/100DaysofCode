7b718f39972b20ffbf6b5da355daa5af8bd5697f
Barbarita Lara <hola@barbaritalara.com>
2022-09-24 22:13:25 -0300

    first commit

---
+++ day1.py
@@ -0,0 +1,10 @@
+# Como contar la cantidad de caracteres 
+# de un nombre ingresado
+
+#print("Hello World")
+#input("dime tu nombre: ")
+#print(len("hola"))
+
+
+print(len(input("Dime tu nombre: ")))
+
---
+++ day2.py
@@ -0,0 +1,13 @@
+# Crearemos una calculadora de propina utilizando 
+# diferentes tipos da datos y que entregue el resultado con dos decimales
+
+print("--- Bienvenido a la calculadora de propina ---\n")
+cuenta = float(input("¿Cuál es el total de la cuenta? $"))
+propina = int(input("¿Cuánto porcentaje de propina desea dar?10, 12 o 15? "))
+personas = int(input("Cuántas personas compartirán la cuenta? "))
+propinaEnProcentaje = propina / 100
+totalPropina = cuenta * propinaEnProcentaje
+totalCuenta = cuenta + totalPropina
+cuentaPorPersona = totalCuenta / personas
+totalFinal = "{:.2f}".format(cuentaPorPersona)
+print(f"Cada persona debe pagar: ${totalFinal}")
