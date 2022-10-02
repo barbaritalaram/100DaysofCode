9ed85a3fa6d565411610a1e04635f30f2e4bdf40
Barbarita Lara <hola@barbaritalara.com>
2022-10-01 21:59:35 -0300

    solución a maze de reeborg.ca

---
+++ day6.py
@@ -0,0 +1,20 @@
+# Si quieres resolver el desafío Maze en reeborg.ca
+# debes ingresar a https://reeborg.ca/reeborg.html
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
